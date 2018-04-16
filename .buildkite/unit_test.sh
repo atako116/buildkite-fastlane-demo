1b1487eb73573a3985f9c1010e14e7c02ef4e891
Daniel Thompson <daniel@venmo.com>
2018-04-16 13:11:25 -0400

    Update the unit test script

--- .buildkite/unit_test.sh
+++ .buildkite/unit_test.sh
@@ -3,16 +3,11 @@
 set -eu
 
 echo "--- :gem: Bundling gems"
-gem install bundler
 bundle install
 
-echo "--- :swift: Installing Swift dependencies"
-rm -rf Carthage/
-bin/fetch
-
 echo "--- :buildkite: Setting version metadata"
-VERSION=$(/usr/libexec/PlistBuddy -c "Print :CFBundleShortVersionString" ../Venmo/Resources/Base.lproj/Info.plist)
+VERSION=$(/usr/libexec/PlistBuddy -c "Print :CFBundleShortVersionString" "../Buildkite Fastlane Demo/Resources/Base.lproj/Info.plist")
 buildkite-agent meta-data set "version" "${VERSION}"
 
 echo "--- :fastlane: Running unit tests"
-bundle exec fastlane scan --scheme Venmo --clean true --skip_slack true
+bundle exec fastlane test
